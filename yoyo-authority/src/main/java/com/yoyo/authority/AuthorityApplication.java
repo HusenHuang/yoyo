a9da186289ee89b029e114a0690fd138771df35e
huangxincheng <huangxincheng@xmiles.cn>
2019-09-04 18:31:36 +0800

    init

---
+++ yoyo-authority/src/main/java/com/yoyo/authority/AuthorityApplication.java
@@ -0,0 +1,25 @@
+package com.yoyo.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/***
+ @Author:MrHuang
+ @Date: 2019/9/4 18:18
+ @DESC: TODO
+ @VERSION: 1.0
+ ***/
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableScheduling
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400)
+public class AuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class);
+    }
+
+}
