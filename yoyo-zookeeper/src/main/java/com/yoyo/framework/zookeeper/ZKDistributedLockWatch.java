5cac523c9879b3ffa98067c0a7cc64f2891c2bf4
huangxincheng <huangxincheng@xmiles.cn>
2019-09-17 16:43:38 +0800

    修改

--- yoyo-zookeeper/src/main/java/com/yoyo/framework/zookeeper/ZKDistributedLockWatch.java
+++ yoyo-zookeeper/src/main/java/com/yoyo/framework/zookeeper/ZKDistributedLockWatch.java
@@ -25,25 +25,21 @@ public class ZKDistributedLockWatch {
     /**
      * 最大重试次数
      */
-    @Value("${spring.zookeeper.maxRetries}")
     private int maxRetries = 3;
 
     /**
      * 初始化睡眠时间
      */
-    @Value("${spring.zookeeper.baseSleepTimeMs}")
     private int baseSleepTimeMs = 1000;
 
     /**
      * Session超时时间
      */
-    @Value("${spring.zookeeper.sessionTimeoutMs}")
     private int sessionTimeoutMs = 60000;
 
     /**
      * Connection超时时间
      */
-    @Value("${spring.zookeeper.connectionTimeoutMs}")
     private int connectionTimeoutMs = 15000;
 
     /**
