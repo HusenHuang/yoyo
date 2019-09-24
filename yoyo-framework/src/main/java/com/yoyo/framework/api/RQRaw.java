270764cfe906648ed0bc00f80f517f657e4679fd
huangxincheng <huangxincheng@xmiles.cn>
2019-09-24 16:44:13 +0800

    修改

--- yoyo-framework/src/main/java/com/yoyo/framework/api/RQRaw.java
+++ yoyo-framework/src/main/java/com/yoyo/framework/api/RQRaw.java
@@ -3,6 +3,7 @@ package com.yoyo.framework.api;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Map;
 
 /***
  @Author:MrHuang
@@ -13,7 +14,16 @@ import java.io.Serializable;
 @Data
 public class RQRaw<T> implements Serializable {
 
+    /**
+     * YOYO_MANAGER:    YOYO后台系统
+     * YOYO_WEB_H5:     YOYO_WEB_H5
+     * YOYO_WX:         YOYO_WX_H5
+     * YOYO_WXA:        YOYO_WXA
+     */
     private String source;
 
+    /**
+     * 请求体数据
+     */
     private T payload;
 }
