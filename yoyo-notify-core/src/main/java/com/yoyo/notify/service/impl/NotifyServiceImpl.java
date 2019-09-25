4af37d3257bc94c199aea405e2c31f2a7826f74b
huangxincheng <huangxincheng@xmiles.cn>
2019-09-25 16:54:53 +0800

    修改模块代码

--- yoyo-notify-core/src/main/java/com/yoyo/notify/service/impl/NotifyServiceImpl.java
+++ yoyo-notify-core/src/main/java/com/yoyo/notify/service/impl/NotifyServiceImpl.java
@@ -1,8 +1,7 @@
 package com.yoyo.notify.service.impl;
 
 import com.yoyo.notify.INotifyService;
-
-import java.util.concurrent.TimeUnit;
+import org.apache.dubbo.config.annotation.Method;
 
 /***
  @Author:MrHuang
@@ -10,7 +9,10 @@ import java.util.concurrent.TimeUnit;
  @DESC: TODO
  @VERSION: 1.0
  ***/
-@org.apache.dubbo.config.annotation.Service(timeout = 10)
+@org.apache.dubbo.config.annotation.Service(
+        timeout = 10,
+        methods = {@Method(name = "echo", timeout = 2)}
+)
 @org.springframework.stereotype.Service
 public class NotifyServiceImpl implements INotifyService {
     @Override
